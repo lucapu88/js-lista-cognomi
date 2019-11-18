fcc631388f89003bf9850f0d4943653c5a62f94b
lucapu88 <caputoluca88@gmail.com>
2019-11-18 16:13:16 +0100

    fine esercizio. da inserire in html

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
 // Scrivi anche la posizione della lista in cui  il nuovo utente si trova.
-var cognome = prompt('Ciao, per favore digita il tuo cognome');
+
+var cognome = prompt('Ciao, per favore digita il tuo cognome'); //chiedo il cognome all'utente
 console.log(cognome);
-var lista = ['pippo', 'pluto', 'martufello', 'paperino', 'topolino', 'minnie'];
+var lista = ['pippo', 'pluto', 'martufello', 'paperino', 'topolino', 'minnie']; //definisco la lista
 console.log(lista);
-lista.push(cognome);
-lista.sort();
+lista.push(cognome); //inserisco il cognome digitato nella lista
+lista.sort(); //stampo la lista in ordine alfabetico
 console.log(lista);
+var posizione = lista.indexOf(cognome) +1;
+console.log(posizione);
